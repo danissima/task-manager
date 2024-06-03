afcc83579afc85acdb3d85ef6d7b0d4e6d5998e8
Danila Odintsov <danila.odintsov01@bk.ru>
2024-06-03 20:07:17 +0400

    feat(master): add eslint and prettier

---
+++ components/AppContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+  .container {
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 0 64px;
+
+    @include break($lg) {
+      padding: 0 32px;
+    }
+
+    @include break($md) {
+      padding: 0 20px;
+    }
+  }
+</style>
